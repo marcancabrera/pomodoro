8ebc12630c5ad89e13dcc14c33e58a408431c0d7
GabrielCT <202000210@est.umss.edu>
2021-08-13 23:04:31 -0400

    Funcionalidad completada,permite pausar empezar y reiniciar

--- src/pages/pomodoro/pomodoroTimer.js
+++ src/pages/pomodoro/pomodoroTimer.js
@@ -4,10 +4,34 @@ sec = 0;
 
 
 var startButton = document.getElementById('startButton'),
-    timerString = document.getElementById('timerString');
+    timerString = document.getElementById('timerString'),
+    body = document.getElementById('body'),
+    stopButton = document.getElementById('stopButton');
 
 
-
+stopButton.addEventListener('click', function () {
+    var mainStatus = body.classList.item(0);
+    console.log(mainStatus);
+    clearInterval(id);
+    id = null;
+    startButton.innerHTML = "START";
+    stopButton.classList.remove('show');
+    sec = 0;
+    switch (mainStatus) {
+        case 'main-1': {
+            timerString.innerHTML = "25:00";
+            break;
+        };
+        case 'main-2': {
+            timerString.innerHTML = "05:00";
+            break;
+        };
+        case 'main-3': {
+            timerString.innerHTML = "15:00";
+            break;
+        };
+    }
+});
 
 startButton.addEventListener('click', function () {
     paused = false;
@@ -16,17 +40,22 @@ startButton.addEventListener('click', function () {
     var opcion = startButton.innerHTML;
     console.log(opcion);
     if (opcion == "START") {
+        startButton.classList.add('pauseButton');
         startButton.innerHTML = "STOP";
+        stopButton.classList.add('show');
         if (id == null) {
             id = setInterval(countBack, 1000);
             console.log(id);
             console.log("id creado");
+
         }
 
     } else if (opcion == "STOP") {
+        startButton.classList.remove('pauseButton');
         clearInterval(id);
         id = null;
         startButton.innerHTML = "START";
+        stopButton.classList.remove('show');
     }
 });
 
@@ -71,60 +100,6 @@ function write() {
 }
 
 
-/////////////////////////////////////////////////////////////
-
-timeA = 1500; // se pueden obtener los valores de algun input
-timeB = 300; // se pueden obtener los valores de algun input
-timeC = 900; // se pueden obtener los valores de algun input
-
-timeInit = timeA;   // al abrir la pagina el time por defecto es el A
-setTimer(timeA);  // se actualiza el texto del tiempo con el tiempo A
-
-
-var timeInit; // tiempo inicial, solo cambia cuando se elige otra opcion
-var timeNow; // tiempo actual, que decrece cuando se apreta START
-
-// cada boton de las 3 opciones envia un valor entre 0 y 2, que corresponden con los 3 times
-function timerOptionOnClick(value){
-    if(value== 0){
-        timeInit = timeA;
-        setTimer(timeA);
-    } else if(value == 1){
-        timeInit = timeB;
-        setTimer(timeB);
-    }else if(value == 2){
-        timeInit = timeC;
-        setTimer(timeC);
-    }
-    timeNow = timeInit;
-}
-
-// Esta función solo convierte los segundos en strings con formato minutos:segundos
-function setTimer(value){
-    value %= 3600;
-    minutes = Math.floor(value / 60);
-    seconds = value % 60;
-    if(seconds < 10){
-        seconds = '0'+seconds;
-    }
-    if(minutes < 10){
-        minutes = '0'+minutes;
-    }
-    document.getElementById('timerValue').textContent = minutes + ":" +seconds; //    <-  ACTUALIZA EL TEXTO TIEMPO
-}
-
-//Esta función solo inicia el conteo regresivo hasta llegar a 0.
-//Todavia no funciona bien porque si se vuelve a llamar el conteo se acelera por alguna razón
-//Talvez se solucione con un booleano
-function startTimer(){
-    setTimeout(() => {
-        if(timeNow > 0){
-            self.startTimer();
-            timeNow -= 1;
-            setTimer(timeNow);
-        }
-    }, 1000);
-}
 
 
 
